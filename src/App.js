76a250d259e786a5faeb06e1b70d8433edac0cdd
Felipe Tun <felipe.tun.cauich@gmail.com>
2024-01-05 22:08:01 -0600

    add profile view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
+import imgMe from "./assets/me.jpeg"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Felipe Tun...
-        </p>
-      </header>
-    </div>
-  );
+	return <section class="py-10 md:py-16">
+
+		<div class="container max-w-screen-xl mx-auto px-4">
+
+			<div class="text-center">
+
+				<div class="flex justify-center mb-16">
+					<img src={imgMe} alt="Image" className='object-contain h-48 w-96' />
+				</div>
+
+				<h6 class="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">Felipe Tun</h6>
+
+				<h1 class="font-normal text-gray-900 text-4xl md:text-7xl leading-none mb-8">Desarrollador de Software</h1>
+
+				<p class="font-normal text-gray-600 text-md md:text-xl mb-16">Tengo 8 años de experiencia en el desarrollo y mantenimiento de software,
+					con herramientas y metodologías agiles Apasionado por el mundo de la programación, me gusta
+					aplicar mis conocimientos y experiencia para ayudar en soluciones a problemas aparentemente
+					complejos
+				</p>
+			</div>
+
+		</div>
+	</section>
 }
 
 export default App;
